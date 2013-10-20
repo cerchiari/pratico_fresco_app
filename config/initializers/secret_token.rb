c5ffa22ac8b791b0a00dc1fcf9d516198bab1a83
Ana Paula Nunes Cerchiari Almeida <aalmeida2014@kellogg.northwestern.edu>
2013-10-19 19:12:52 -0500

    rails news
    Generate brand new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PraticoFrescoApp::Application.config.secret_key_base = 'bfa7287be6fd8bf6cf0c824f26ee64b7d3dd72b9143def74ef5a6410cf8d1c37a4521e95b588deb77b8076a26b971706888ee17e19e7766b1bfe10bc67ab5538'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PraticoFrescoApp::Application.config.session_store :cookie_store, key: '_pratico_fresco_app_session'
